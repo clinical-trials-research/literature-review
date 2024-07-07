2c05926c74f3c471f697748c646e494ff04472ec
wuihee <wuihee@gmail.com>
2024-07-07 13:32:33 +0800

    Updated database

--- scripts/find_missing_fields.py
+++ scripts/find_missing_fields.py
@@ -1,9 +1,11 @@
 import json
 
-with open("./files/sample_processed.json") as f:
+with open("./files/test.json") as f:
     structure = json.load(f)
 
-study = {key: value for key, value in structure.items() if not isinstance(value, list)}
+structure = {
+    key: value for key, value in structure.items() if not isinstance(value, list)
+}
 
-with open("./files/test.json", "w") as f:
-    json.dump(study, f)
+with open("./files/test2.json", "w") as f:
+    json.dump(structure, f)
