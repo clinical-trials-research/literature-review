84a7eddf82ad02003834ef11916eddea053d35e2
wuihee <wuihee@gmail.com>
2024-07-20 15:04:13 +0800

    Updated scripts

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -1,4 +1,4 @@
 from litreview import ClinicalTrials
 
-trials = ClinicalTrials(num_studies=100, schema_directory="files/schema.json")
+trials = ClinicalTrials(schema_directory="files/schema.json")
 trials.update_database()
